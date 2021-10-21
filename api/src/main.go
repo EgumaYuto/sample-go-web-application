64165e8bcb6bc3949b1d87814529a4569b316f89
yuto.eguma <yuto.eguma@gmail.com>
2021-10-22 00:05:10 +0900

    helloを返すアプリケーションの実装

---
+++ api/src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/hello", hello)
+	server.ListenAndServe()
+}
