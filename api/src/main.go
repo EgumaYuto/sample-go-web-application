a6d4c0eb609de0bf4f403578641cb31d16d96312
yuto.eguma <yuto.eguma@gmail.com>
2021-10-22 00:55:34 +0900

    golang のサンプル web app 作成

--- api/src/main.go
+++ api/src/main.go
@@ -11,7 +11,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr: ":8080",
 	}
 	http.HandleFunc("/hello", hello)
 	server.ListenAndServe()
