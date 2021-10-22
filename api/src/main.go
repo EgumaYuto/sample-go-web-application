6c30acf2bcac04b5cb3b1064c8a310db4463f736
yuto.eguma <yuto.eguma@gmail.com>
2021-10-22 17:01:10 +0900

    ヘルスチェック用エンドポイントを追加

--- api/src/main.go
+++ api/src/main.go
@@ -9,10 +9,15 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This service is healthy.")
+}
+
 func main() {
 	server := http.Server{
 		Addr: ":8080",
 	}
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/health", health)
 	server.ListenAndServe()
 }
