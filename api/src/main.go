b3de8bf41c0bceb98d47956fac113b112064bbbb
yuto.eguma <yuto.eguma@gmail.com>
2021-10-22 18:55:56 +0900

    データベースへのヘルスチェック

--- api/src/main.go
+++ api/src/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("mysql", "root:example@tcp(db:3306)/test_db")
+	if err != nil {
+		panic(err)
+	}
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This service is healthy.")
+	_, err := Db.Exec("select 1")
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "Error!!")
+	} else {
+		fmt.Fprintf(w, "OK!")
+	}
 }
 
 func main() {
